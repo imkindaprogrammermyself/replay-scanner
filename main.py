7c75ecdc0df46e9ce561287b2f723507701b9ed6
notyourfather <applicatnccnt@gmail.com>
2022-05-29 19:37:27 +0800

    Whole replay file scanner.

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
-from utils import LOGGER
 from replay_reader import ReplayReader
-from concurrent.futures import ThreadPoolExecutor
 
-search = [b'cnt\n', b'ccommands\n', b'cwebbrowser\n', b'c__builtin__\nexec', b'c__builtin__\neval',
-          b'c__builtin__\nexecfile']
+MODULES = [b'cnt\n', b'ccommands\n', b'cwebbrowser\n', b'c__builtin__\nexec', b'c__builtin__\neval',
+           b'c__builtin__\nexecfile']
 
 
-def scanner(a):
-    _replay_dat, _search_str = a
-    _pos = 0
-    while _pos + 1 != len(_replay_dat):
-        if _replay_dat[_pos:_pos + len(_search_str)] == _search_str:
-            return True, _search_str
-        _pos += 1
-    return False, _search_str
+class ReplayScanner:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def scan(data: bytes, prohibited_modules: list[bytes]) -> list[tuple[bool, bytes]]:
+        result: list[tuple[bool, bytes]] = []
+        position = 0
+        while position + 1 != len(data):
+            for module in prohibited_modules:
+                if data[position:position + len(module)] == module:
+                    result.append((True, module))
+            position += 1
+        return result
 
 
 if __name__ == '__main__':
+    import time
     rr = ReplayReader()
-    dat = rr.read_replay("replays/infected.wowsreplay")
+    rs = ReplayScanner()
+    replay_data = rr.read_replay("replays/infected.wowsreplay")
 
-    with ThreadPoolExecutor() as tpe:
-        for result in tpe.map(scanner, [(dat, s) for s in search]):
-            is_infected, infected_by = result
-            if is_infected:
-                LOGGER.info(f"INFECTED: {is_infected}, INFECTED BY: {infected_by}")
+    start = time.perf_counter()
+    scan_result = rs.scan(replay_data, MODULES)
+    end = time.perf_counter()
+    print(end-start)
+    print(scan_result)
